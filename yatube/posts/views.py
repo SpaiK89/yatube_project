9e6e9100f8b78b57a2f5b34b530d8517395807f6
Игорь Богомолов <spaik111@gmail.com>
2022-11-03 19:12:22 +0300

    Final version

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post, Group
+
+
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {'text': text, 'posts': posts,}
+    return HttpResponse(render(request, template, context))
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Сообщения {slug}')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {'group': group, 'posts': posts}
+    return HttpResponse(render(request, template, context))
